be2e1ca09517087af951ea6d6d0955758753a2cf
Arthur Khrabrov <monterrey.xdd@gmail.com>
2016-11-12 01:27:21 +0300

    Fix README

---
+++ ITMONavServer/src/ITMONavServer.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITMONavServer.WebApi.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from ITMONavServer!";
+        }
+    }
+}
